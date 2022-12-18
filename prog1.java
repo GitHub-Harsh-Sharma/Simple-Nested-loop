f21d8d24618c7f80ca95252700e8d94c644cacd8
Harsh Sharma <117378407+GitHub-Harsh-Sharma@users.noreply.github.com>
2022-12-18 12:38:30 +0530

    Add files via upload

---
+++ prog1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class Prog1nested
+{
+public static void main(String args[])
+{
+for(int i=1;i<=2;i++)
+{
+  for(int k=1;k<=3;k++)
+  {
+  System.out.print("Harsh ");
+  }
+  System.out.println("");
+}
+}
+}
